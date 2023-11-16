3076c4570a8e37af2aa74eaa19f90202a71e360e
mathiszerari <mathiszerari@icloud.com>
2023-11-16 20:38:57 +0100

    (new) github data & redirection done

--- api/auth-github.py
+++ api/auth-github.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 import httpx
 from starlette.responses import RedirectResponse
 
@@ -12,16 +12,16 @@ async def github_login():
     return RedirectResponse(f'https://github.com/login/oauth/authorize?client_id={github_client_id}', status_code=303)
 
 @app.get("/api/github-code")
-async def github_code(code: str):
+async def github_code(code: str, request: Request = str()):
     params = {
         'client_id': github_client_id,
         'client_secret': github_client_secret,
         'code': code
     }
     headers = {'Accept': 'application/json'}
-    async with httpx.AsyncClient() as client:  # Correction ici
+    async with httpx.AsyncClient() as client:
         response = await client.post(
-            'https://github.com/login/oauth/access_token', params=params, headers=headers  # Correction ici
+            'https://github.com/login/oauth/access_token', params=params, headers=headers
         )
     response_json = response.json()
     access_token = response_json.get('access_token')
@@ -29,4 +29,12 @@ async def github_code(code: str):
     async with httpx.AsyncClient() as client:
         headers.update({'Authorization' : f'Bearer {access_token}'})
         response = await client.get('https://api.github.com/user', headers=headers)
-    return response.json()
+      
+    user_data = response.json()
+    print(user_data)
+    
+    # Remplacez "http://localhost:4200" par le chemin/route correcte de votre application Angular
+    redirect_url = "http://localhost:4200/"
+    
+    # Utilisez RedirectResponse pour rediriger l'utilisateur
+    return RedirectResponse(url=redirect_url + user_data['login'], status_code=303)

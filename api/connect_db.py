97ad4007f26ddda0fee3e2dcb7584d90695447a9
Mathis Zerari <mathiszerari@icloud.com>
2024-08-09 00:06:00 -0400

    :bug: use mongo base url

--- api/connect_db.py
+++ api/connect_db.py
@@ -1,29 +1,12 @@
 from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 from dotenv import load_dotenv
-import os
-import ssl
-import certifi
-import logging
-
-logging.basicConfig(level=logging.DEBUG)
-
-def connect_to_database():
-    load_dotenv()
-    
-    uri = os.getenv("url")
-    logging.debug(f"MongoDB URI: {uri}")
-
-    try:
-        client = MongoClient(uri, ssl=True, ssl_cert_reqs=ssl.CERT_REQUIRED,
-                             ssl_ca_certs=certifi.where(), ssl_version=ssl.PROTOCOL_TLSv1_2)
-        logging.debug("Client created successfully")
-        
-        db = client.get_database()
-        logging.debug(f"Got database: SHIPFASTER")
-        
-        client.admin.command('ping')
-        logging.info("Pinged your deployment. You successfully connected to MongoDB!")
-        return client
-    except Exception as e:
-        logging.error(f"Failed to connect to MongoDB: {e}")
-        raise
+url = os.getenv("url")
+# Create a new client and connect to the server
+client = MongoClient(url, server_api=ServerApi('1'))
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)

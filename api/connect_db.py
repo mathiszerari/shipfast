77d316e20c03c35cb04e750bc552f87f462533bb
Mathis Zerari <mathiszerari@icloud.com>
2024-08-08 23:20:01 -0400

    :bug: debug mongo db connexion

--- api/connect_db.py
+++ api/connect_db.py
@@ -1,6 +1,7 @@
 from pymongo.mongo_client import MongoClient
 from dotenv import load_dotenv
 import os
+import ssl
 
 def connect_to_database():
     load_dotenv()
@@ -8,12 +9,16 @@ def connect_to_database():
     uri = os.getenv("url")
     print(uri)
 
-    client = MongoClient(uri)
+    # Ajoutez ces options SSL
+    ssl_cert_reqs = ssl.CERT_REQUIRED
+    ssl_ca_certs = certifi.where()
+
+    client = MongoClient(uri, ssl=True, ssl_cert_reqs=ssl_cert_reqs, ssl_ca_certs=ssl_ca_certs)
 
     try:
         client.admin.command('ping')
         print("Pinged your deployment. You successfully connected to MongoDB!")
         return client
     except Exception as e:
-        print(e)
+        print(f"An error occurred while connecting to MongoDB: {e}")
         raise
